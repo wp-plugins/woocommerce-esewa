b0824c0be33420afcf0aadd96484f08400892c22
axisthemes <axisthemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-12 06:06:10 +0000

    Deploy from Git (1.0.0)
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-esewa/trunk@1158457 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/includes/class-wc-gateway-esewa-request.php
@@ -0,0 +1,99 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Generates requests to send to eSewa
+ */
+class WC_Gateway_eSewa_Request {
+
+	/**
+	 * Pointer to gateway making the request
+	 * @var WC_Gateway_eSewa
+	 */
+	protected $gateway;
+
+	/**
+	 * Endpoint for requests from eSewa
+	 * @var string
+	 */
+	protected $notify_url;
+
+	/**
+	 * Constructor
+	 * @param WC_Gateway_eSewa $gateway
+	 */
+	public function __construct( $gateway ) {
+		$this->gateway    = $gateway;
+		$this->notify_url = WC()->api_request_url( 'WC_Gateway_eSewa' );
+	}
+
+	/**
+	 * Get the eSewa request URL for an order
+	 * @param  WC_Order $order
+	 * @param  boolean  $sandbox
+	 * @return string
+	 */
+	public function get_request_url( $order, $sandbox = false ) {
+		$esewa_args = http_build_query( $this->get_esewa_args( $order ), '', '&' );
+
+		if ( $sandbox ) {
+			return 'https://dev.esewa.com.np/epay/main?' . $esewa_args;
+		} else {
+			return 'https://esewa.com.np/epay/main?' . $esewa_args;
+		}
+	}
+
+	/**
+	 * Get eSewa Args for passing to eSewa
+	 * @param  WC_Order $order
+	 * @return array
+	 */
+	protected function get_esewa_args( $order ) {
+		WC_Gateway_eSewa::log( 'Generating payment form for order ' . $order->get_order_number() . '. Notify URL: ' . $this->notify_url );
+
+		return apply_filters( 'woocommerce_esewa_args', array(
+			'amt'   => wc_format_decimal( $this->get_order_subtotal( $order ), 2 ),
+			'txAmt' => wc_format_decimal( $order->get_total_tax(), 2 ),
+			'pdc'   => wc_format_decimal( $order->get_total_shipping(), 2 ),
+			'psc'   => wc_format_decimal( $this->get_service_charge( $order ), 2 ),
+			'tAmt'  => wc_format_decimal( $order->get_total(), 2 ),
+			'scd'   => $this->gateway->get_option( 'service_code' ),
+			'pid'   => $this->gateway->get_option( 'invoice_prefix' ) . $order->get_order_number(),
+			'su'    => add_query_arg( array( 'payment_status' => 'success', 'key' => $order->order_key ), $this->notify_url ),
+			'fu'    => add_query_arg( array( 'payment_status' => 'failure', 'key' => $order->order_key ), $this->notify_url ),
+		), $order );
+	}
+
+	/**
+	 * Get the subtotal to send to eSewa
+	 * @param  WC_Order $order
+	 * @return float
+	 */
+	private function get_order_subtotal( $order ) {
+		$subtotal = 0;
+		foreach ( $order->get_items() as $item ) {
+			$subtotal += ( isset( $item['line_subtotal'] ) ) ? $item['line_subtotal'] : 0;
+		}
+
+		return $subtotal;
+	}
+
+	/**
+	 * Get the service charge to send to eSewa
+	 * @param  WC_Order $order
+	 * @return float
+	 */
+	private function get_service_charge( $order ) {
+		$charge = 0;
+		if ( sizeof( $order->get_fees() ) > 0 ) {
+			foreach ( $order->get_fees() as $item ) {
+				$charge += ( isset( $item['line_total'] ) ) ? $item['line_total'] : 0;
+			}
+		}
+
+		return $charge;
+	}
+}
---
+++ woocommerce-esewa.php
@@ -0,0 +1,134 @@
+<?php
+/**
+ * Plugin Name: WooCommerce eSewa
+ * Plugin URI: https://github.com/axisthemes/woocommerce-esewa
+ * Description: WooCommerce eSewa is a Nepali payment gateway for WooCommerce.
+ * Author: AxisThemes
+ * Author URI: http://axisthemes.com
+ * Version: 1.0.0
+ * License: GPLv2 or later
+ * Text Domain: woocommerce-esewa
+ * Domain Path: /languages/
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+if ( ! class_exists( 'WC_eSewa' ) ) :
+
+/**
+ * WooCommerce eSewa main class.
+ */
+class WC_eSewa {
+
+	/**
+	 * Plugin version.
+	 *
+	 * @var string
+	 */
+	const VERSION = '1.0.0';
+
+	/**
+	 * Instance of this class.
+	 *
+	 * @var object
+	 */
+	protected static $instance = null;
+
+	/**
+	 * Initialize the plugin.
+	 */
+	private function __construct() {
+		// Load plugin text domain
+		add_action( 'init', array( $this, 'load_plugin_textdomain' ) );
+
+		// Checks with WooCommerce is installed.
+		if ( defined( 'WC_VERSION' ) && version_compare( WC_VERSION, '2.3', '>=' ) ) {
+			$this->includes();
+
+			// Hooks
+			add_filter( 'woocommerce_payment_gateways', array( $this, 'add_gateway' ) );
+			add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), array( __CLASS__, 'plugin_action_links' ) );
+		} else {
+			add_action( 'admin_notices', array( $this, 'woocommerce_missing_notice' ) );
+		}
+	}
+
+	/**
+	 * Return an instance of this class.
+	 *
+	 * @return object A single instance of this class.
+	 */
+	public static function get_instance() {
+		// If the single instance hasn't been set, set it now.
+		if ( null == self::$instance ) {
+			self::$instance = new self;
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 * Get the plugin path.
+	 *
+	 * @return string
+	 */
+	public function plugin_path() {
+		return untrailingslashit( plugin_dir_path( __FILE__ ) );
+	}
+
+	/**
+	 * Load the plugin text domain for translation.
+	 */
+	public function load_plugin_textdomain() {
+		$locale = apply_filters( 'plugin_locale', get_locale(), 'woocommerce-esewa' );
+
+		load_textdomain( 'woocommerce-esewa', trailingslashit( WP_LANG_DIR ) . 'woocommerce-esewa/woocommerce-esewa-' . $locale . '.mo' );
+		load_plugin_textdomain( 'woocommerce-esewa', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+	}
+
+	/**
+	 * Includes.
+	 */
+	private function includes() {
+		include_once( 'includes/class-wc-esewa-gateway.php' );
+	}
+
+	/**
+	 * Add the gateway to WooCommerce.
+	 *
+	 * @param  array $methods WooCommerce payment methods.
+	 * @return array          Payment methods with eSewa.
+	 */
+	public function add_gateway( $methods ) {
+		$methods[] = 'WC_Gateway_eSewa';
+		return $methods;
+	}
+
+	/**
+	 * Show action links on the plugin screen.
+	 * @param  mixed $links Plugin Action links
+	 * @return array
+	 */
+	public static function plugin_action_links( $links ) {
+		$action_links = array(
+			'settings' => '<a href="' . admin_url( 'admin.php?page=wc-settings&tab=checkout&section=wc_gateway_esewa' ) . '" title="' . esc_attr( __( 'View Settings', 'woocommerce-esewa' ) ) . '">' . __( 'Settings', 'woocommerce-esewa' ) . '</a>',
+		);
+
+		return array_merge( $action_links, $links );
+	}
+
+	/**
+	 * WooCommerce fallback notice.
+	 *
+	 * @return string
+	 */
+	public function woocommerce_missing_notice() {
+		echo '<div class="error notice is-dismissible"><p>' . sprintf( __( 'WooCommerce eSewa depends on the last version of %s or later to work!', 'woocommerce-esewa' ), '<a href="http://www.woothemes.com/woocommerce/" target="_blank">' . __( 'WooCommerce 2.3', 'woocommerce-esewa' ) . '</a>' ) . '</p></div>';
+	}
+}
+
+add_action( 'plugins_loaded', array( 'WC_eSewa', 'get_instance' ), 0 );
+
+endif;
